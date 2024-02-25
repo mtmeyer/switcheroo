764a0d3561870c5c28cb1442781120c4acfb9f38
Michael Meyer <9434150+mtmeyer@users.noreply.github.com>
2024-02-26 10:01:16 +1100

    feat: add basic fuzzy find functionality

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"switcheroo/utils"
+
+	"github.com/ktr0731/go-fuzzyfinder"
 )
 
 func main() {
@@ -15,5 +17,24 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(directories)
+	// Initialise fuzzy finder
+	selectedIndex, err := fuzzyfinder.Find(
+		directories,
+		func(i int) string {
+			return fmt.Sprintf("%s", directories[i].Name)
+		},
+		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+
+			return fmt.Sprintf("TODO: metadata")
+		}),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Selected index: %d", selectedIndex)
 }
--- utils/directory.go
+++ utils/directory.go
@@ -2,38 +2,57 @@ package utils
 
 import (
 	"os"
+	"path"
+	"sort"
+	"strings"
 )
 
-func GetDirectoryContents(directory string) ([]string, error) {
+type Directory struct {
+	Group string
+	Path  string
+	Name  string
+}
+
+func GetSingleDirectoryContents(directory string, group string) ([]Directory, error) {
 	// TODO: Allow the directory to be defined from $HOME path, not just absolute
 	dirContents, err := os.ReadDir(directory)
 	if err != nil {
 		return nil, err
 	}
 
-	directories := []string{}
+	directories := []Directory{}
 
 	for _, dir := range dirContents {
 		if dir.IsDir() {
-			directories = append(directories, dir.Name())
+			directories = append(directories, Directory{Name: dir.Name(), Group: group, Path: path.Join(directory, dir.Name())})
 		}
 	}
 
 	return directories, nil
 }
 
-func GetAllDirectoryContents(directories map[string]string) (map[string][]string, error) {
-	allDirectoryContents := map[string][]string{}
+func GetAllDirectoryContents(directories map[string]string) ([]Directory, error) {
+	allDirectoryContents := []Directory{}
 
-	for name, path := range directories {
-		dirContents, err := GetDirectoryContents(path)
+	for group, path := range directories {
+		dirContents, err := GetSingleDirectoryContents(path, group)
 
 		if err != nil {
 			return nil, err
 		}
 
-		allDirectoryContents[name] = dirContents
+		allDirectoryContents = append(allDirectoryContents, dirContents...)
 	}
+
+	// Sort directories into alphabetical order based on name
+	sort.Slice(allDirectoryContents, func(i, j int) bool {
+		comparison := strings.Compare(allDirectoryContents[i].Name, allDirectoryContents[j].Name)
+		if comparison == -1 {
+			return true
+		} else {
+			return false
+		}
+	})
 
 	return allDirectoryContents, nil
 }
